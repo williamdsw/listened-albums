4cc25d9ee8d3d053e84d54e1e24dd61e229d2c95
William Santos <williamdsw@outlook.com>
2020-01-11 14:04:22 -0300

    Added JQuery, bootstrap.js, merged albums list in year's list

---
+++ js/data2020.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let albums2020 =
+{
+    year: 2020,
+    content: 
+    [
+        new Album ({
+            name: "Homework",
+            artist: "Daft Punk",
+            genre: "House",
+            releaseYear: 1997,
+            cover: "covers/daft-punk--homework.png",
+            spotifyLink: "https://open.spotify.com/album/5uRdvUR7xCnHmUW8n64n9y",
+        }),
+        new Album ({
+            name: "Discovery",
+            artist: "Daft Punk",
+            genre: "Disco",
+            releaseYear: 2001,
+            cover: "covers/daft-punk--discovery.png",
+            spotifyLink: "https://open.spotify.com/album/2noRn2Aes5aoNVsU6iWThc",
+        }),
+        new Album ({
+            name: "Human After All",
+            artist: "Daft Punk",
+            genre: "Dance-rock",
+            releaseYear: 2005,
+            cover: "covers/daft-punk--human-after-all.png",
+            spotifyLink: "https://open.spotify.com/album/2T7DdrOvsqOqU9bGTkjBYu",
+        }),
+        new Album ({
+            name: "Random Access Memories",
+            artist: "Daft Punk",
+            genre: "Disco",
+            releaseYear: 2013,
+            cover: "covers/daft-punk--random-access-memories.png",
+            spotifyLink: "https://open.spotify.com/album/4m2880jivSbbyEGAKfITCa",
+        }),
+    ]
+}
--- js/listened-albums.js
+++ js/listened-albums.js
@@ -4,16 +4,17 @@
 // ELEMENTS
 
 const albumHeader = document.querySelector ('#albums_header');
-const container = document.querySelector ('#albums_container');
+const yearContainer = document.querySelector ('#year_container');
 const keywordsInput = document.querySelector ("#keywords_input");
 const propertySelect = document.querySelector ("#property_select");
 
 //-------------------------------------------------------------------//
 // EVENTS
 
-window.addEventListener ("load", function ()
+$(document).ready (() =>
 {
-    albums = albums.reverse ();
+    albums2019.content = albums2019.content.reverse ();
+    albums2020.content = albums2020.content.reverse ();
 
     keywordsInput.addEventListener ("keyup", function ()
     {
@@ -26,18 +27,39 @@ window.addEventListener ("load", function ()
     });
 
     albumHeader.innerHTML = "Latest entries:";
-    listAll (albums);
+    listAll (albums2020, true);
+    listAll (albums2019, false);
+    bindHeadersClick ();
 });
 
 //-------------------------------------------------------------------//
 // HELPER FUNCTIONS
 
-function listAll (albums)
+function listAll (albums, toShow)
 {
-    container.innerHTML = "";
-    albums.forEach ((album, index) => 
+    let contentLength = albums.content.length;
+    // New year container
+    yearContainer.innerHTML += 
+    `<div id="year_accordion_${albums.year}" class="accordion">
+        <div class="card">
+            <div id="accordion_header_${albums.year}" class="card-header">
+                <h1 class="mb-0"> ${albums.year} - ${contentLength >= 1 ? contentLength + ' album(s) ' : 'Not found' } </h1>
+            </div>
+
+            <div id="accordion_content_${albums.year}" class="card-content collapse ${toShow ? 'show' : ''}">
+                <div class="card-body">
+                    <div id="albums_container_${albums.year}" class="row"></div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+    // List albums content
+    let albumContainer = document.querySelector ('#albums_container_' + albums.year);
+    albumContainer.innerHTML = "";
+    albums.content.forEach ((album, index) => 
     {
-        container.innerHTML +=
+        albumContainer.innerHTML +=
         `<div class="album col-lg-3 col-md-3 col-xs-6 p-2">
             <div class="hover-effect">
                 <img src="${album.cover}" class="card-img-top"/>
@@ -57,9 +79,25 @@ function listAll (albums)
 
 function filterList (property, searchString)
 {
-    let filtered = albums.filter (album => album[property].toString ().toLowerCase ().includes (searchString.toLowerCase ()));
-    albumHeader.innerHTML = (filtered.length >= 1 ? `${filtered.length} occurrence(s) found with '${searchString}'` : 'Nothing was found');
-    listAll (filtered);
+    let filtered2020 = getFiltered (albums2020, property, searchString);
+    let filtered2019 = getFiltered (albums2019, property, searchString);
+    let total = (filtered2020.content.length + filtered2019.content.length);
+    yearContainer.innerHTML = "";
+    albumHeader.innerHTML = (total >= 1 ? `${total} occurrence(s) found with '${searchString}'` : 'Nothing was found');
+    listAll (filtered2020, true);
+    listAll (filtered2019, true);
+    bindHeadersClick ();
+}
+
+function getFiltered (albums, property, searchString)
+{
+    let filtered = 
+    {
+        year: albums.year,
+        content: albums.content.filter (album => album[property].toString ().toLowerCase ().includes (searchString.toLowerCase ()))
+    };
+
+    return filtered;
 }
 
 function triggerSearch (value)
@@ -72,7 +110,23 @@ function triggerSearch (value)
     }
     else
     {
+        yearContainer.innerHTML = "";
         albumHeader.innerHTML = "Latest entries:";
-        listAll (albums);
+        listAll (albums2020, true);
+        listAll (albums2019, false);
+        bindHeadersClick ();
     }
+}
+
+function bindHeadersClick ()
+{
+    let headers = document.querySelectorAll ("div.accordion .card-header");
+    headers.forEach ((header, index) => 
+    {
+        let content = header.parentElement.querySelector (".card-content");
+        $(header).on ("click", () => 
+        {
+            $(content).toggle ("slow", null);
+        });
+    });
 }
