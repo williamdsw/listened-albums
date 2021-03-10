e3dda6237ec6e1616beee0946bec9e57f13bcbce
William Santos <williamdsw@outlook.com>
2021-03-10 10:39:08 -0300

    :pencil: Ecco The Dolphin by Spencer Nilsen

--- js/listened-albums.js
+++ js/listened-albums.js
@@ -2,43 +2,42 @@
 
 // ELEMENTS
 
-const albumHeader = document.querySelector ('#albums_header');
-const yearContainer = document.querySelector ('#year_container');
-const keywordsInput = document.querySelector ("#keywords_input");
-const propertySelect = document.querySelector ("#property_select");
+const albumHeader = document.querySelector('#albums_header');
+const yearContainer = document.querySelector('#year_container');
+const keywordsInput = document.querySelector("#keywords_input");
+const propertySelect = document.querySelector("#property_select");
 let listYears = [];
 const dataUrls = [
-    'js/data/2019.json', 
+    'js/data/2019.json',
     'js/data/2020.json',
     'js/data/2021.json',
 ];
 
 // EVENTS
 
-$(document).ready (() => {
-    loadJsonData (0);
+$(document).ready(() => {
+    loadJsonData(0);
 
-    keywordsInput.addEventListener ('keyup', function () {
-        triggerSearch (this.value);
+    keywordsInput.addEventListener('keyup', function () {
+        triggerSearch(this.value);
     });
 
-    propertySelect.addEventListener ('change', () => triggerSearch (keywordsInput.value));
+    propertySelect.addEventListener('change', () => triggerSearch(keywordsInput.value));
 
     albumHeader.innerHTML = 'Latest entries:';
-    bindHeadersClick ();
+    bindHeadersClick();
 });
 
 // HELPER FUNCTIONS
 
-function loadJsonData (index) {
+function loadJsonData(index) {
 
     if (dataUrls.length === index) {
         listYears = listYears.reverse();
-        listYears.forEach ((item, currentIndex) => {
-            listAll (item, (currentIndex == 0));
+        listYears.forEach((item, currentIndex) => {
+            listAll(item, (currentIndex == 0));
         });
 
-        console.info('listYears', listYears);
         bindHeadersClick();
         return;
     }
@@ -58,15 +57,15 @@ function loadJsonData (index) {
     .catch(error => console.error('Do something with error', error));
 }
 
-function listAll (item, toShow) {
+function listAll(item, toShow) {
     let contentLength = item.albums.length;
 
     // New year container
-    yearContainer.innerHTML += 
+    yearContainer.innerHTML +=
     `<div id="year_accordion_${item.year}" class="accordion">
         <div class="card">
             <div id="accordion_header_${item.year}" class="card-header">
-                <h1 class="mb-0"> ${item.year} - ${contentLength >= 1 ? contentLength + ' album(s) ' : 'Not found' } </h1>
+                <h1 class="mb-0"> ${item.year} - ${contentLength >= 1 ? contentLength + ' album(s) ' : 'Not found'} </h1>
             </div>
 
             <div id="accordion_content_${item.year}" class="card-content collapse ${toShow ? 'show' : ''}">
@@ -78,12 +77,13 @@ function listAll (item, toShow) {
     </div>`;
 
     // List albums content
-    let albumContainer = document.querySelector ('#albums_container_' + item.year);
+    let albumContainer = document.querySelector('#albums_container_' + item.year);
     albumContainer.innerHTML = '';
-    item.albums.forEach ((album) =>  {
+    item.albums.forEach((album) => {
+        const fullDescription = `${album.artist} - ${album.name}`;
         albumContainer.innerHTML +=
         `<div class="album col-lg-3 col-md-3 col-xs-6 col-sm-6 p-2">
-            <div class="hover-effect">
+            <div class="hover-effect" title="${fullDescription}">
                 <img src="${album.cover}" class="card-img-top"/>
                 <div class="overlay">
                     <div class="info">
@@ -99,48 +99,48 @@ function listAll (item, toShow) {
     });
 }
 
-function filterList (property, searchString) {
+function filterList(property, searchString) {
     let filteredYears = [];
     let total = 0;
-    listYears.forEach ((item) => {
-        let filtered = getFiltered (item, property, searchString);
-        filteredYears.push (filtered);
+    listYears.forEach((item) => {
+        let filtered = getFiltered(item, property, searchString);
+        filteredYears.push(filtered);
         total += filtered.albums.length;
     });
 
     yearContainer.innerHTML = '';
     albumHeader.innerHTML = (total >= 1 ? `${total} occurrence(s) found including '${searchString}'` : 'Nothing was found');
-    filteredYears.forEach ((item) => listAll(item, true));
+    filteredYears.forEach((item) => listAll(item, true));
 
-    bindHeadersClick ();
+    bindHeadersClick();
 }
 
-function getFiltered (item, property, searchString) {
+function getFiltered(item, property, searchString) {
     return {
         year: item.year,
-        albums: item.albums.filter (album => album[property].toString ().toLowerCase ().includes (searchString.toLowerCase ()))
+        albums: item.albums.filter(album => album[property].toString().toLowerCase().includes(searchString.toLowerCase()))
     };
 }
 
-function triggerSearch (value) {
+function triggerSearch(value) {
     if (value !== '') {
-        let property = propertySelect[propertySelect.selectedIndex].value;
-        let searchString = value;
-        filterList (property, searchString);
+        const property = propertySelect[propertySelect.selectedIndex].value;
+        const searchString = value;
+        filterList(property, searchString);
     }
     else {
         yearContainer.innerHTML = '';
         albumHeader.innerHTML = 'Latest entries:';
-        listYears.forEach ((item, index) => listAll (item, (index === 0)));
+        listYears.forEach((item, index) => listAll(item, (index === 0)));
 
-        bindHeadersClick ();
+        bindHeadersClick();
     }
 }
 
-function bindHeadersClick () {
-    let headers = document.querySelectorAll ('div.accordion .card-header');
-    headers.forEach ((header) => {
-        let content = header.parentElement.querySelector ('.card-content');
-        $(header).on ('click', () => $(content).toggle ('slow', null));
+function bindHeadersClick() {
+    const headers = document.querySelectorAll('div.accordion .card-header');
+    headers.forEach((header) => {
+        let content = header.parentElement.querySelector('.card-content');
+        $(header).on('click', () => $(content).toggle('slow', null));
     });
 }
